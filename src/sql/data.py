21b710ba635ec4d6426736dd09596b0684b78a11
Christiano Piccinin <christiano.piccinin@gmail.com>
2023-03-01 14:12:42 -0300

    add new file sql

--- src/sql/data.py
+++ src/sql/data.py
@@ -2,15 +2,35 @@ from pyspark.sql import SparkSession
 spark = SparkSession.builder.getOrCreate()
 
 spark.sql("""
-CREATE TEMPRARY VIEW vw_device
+CREATE TEMPORARY VIEW vw_device
 USING org.apache.spark.sql.json 
-OPTIONS (path "/spark-series/docs/device/*.json")
+OPTIONS (path "C:/git/spark-series/docs/file/device/*.json")
 """)
 
 spark.sql("""
-CREATE TEMPRARY VIEW vw_subscription
+CREATE TEMPORARY VIEW vw_subscription
 USING org.apache.spark.sql.json
-OPTIONS (path "/spark-series/docs/subscription/*.json")
+OPTIONS (path "C:/git/spark-series/docs/file/device/*.json")
 """)
 
 print(spark.catalog.listTables())
+
+
+spark.sql(""" SELECT * FROM vw_device LIMIT 10""").show()
+spark.sql(""" SELECT * FROM vw_subscription LIMIT 10""").show()
+
+join_datasets = spark.sql("""
+                          SELECT dev.user_id,
+                                 dev.model,
+                                 dev.platform, 
+                                 subs.payment_method, 
+                                 subs.plan
+                            FROM vw_device AS dev
+                            INNER JOIN vw_subscription AS subs
+                            ON dev.user_id = subs.user_id
+                            LIMIT 10;
+                          """)
+
+join_datasets.show()
+join_datasets.printSchema()
+join_datasets.count()

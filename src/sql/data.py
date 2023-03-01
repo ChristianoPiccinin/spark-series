2dd5bd176da7f6f97d2914cca693c65c5bcee971
Christiano Piccinin <christiano.piccinin@gmail.com>
2023-03-01 10:24:51 -0300

    Update data.py

--- src/sql/data.py
+++ src/sql/data.py
@@ -1,4 +1,16 @@
 from pyspark.sql import SparkSession
 spark = SparkSession.builder.getOrCreate()
 
-spark.sql()
+spark.sql("""
+CREATE TEMPRARY VIEW vw_device
+USING org.apache.spark.sql.json 
+OPTIONS (path "/spark-series/docs/device/*.json")
+""")
+
+spark.sql("""
+CREATE TEMPRARY VIEW vw_subscription
+USING org.apache.spark.sql.json
+OPTIONS (path "/spark-series/docs/subscription/*.json")
+""")
+
+print(spark.catalog.listTables())
